1f3d57e19cc328b51a4d8e3f623dd845486aba3b
maxwell-wayne-17 <mwayne@alarm.com>
2024-04-09 12:51:58 -0400

    Successfully established a server connection

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ var Data = require('./basicSchema')
 mongoose.connect("mongodb://localhost/newDB")
 
 mongoose.connection.once("open", () => {
-    console.log("Connected to DB")
+    console.log("Connected to DB.")
 }).on("error", (error) => {
-    console.log("Failed to connect " + error)
+    console.log("Failed to connect " + error + ".")
 })
 
+// GET request will enable use to send info from server to the client
+
+
+// POST request will enable us to send info from client to server
+
+// http://<IPv4>:8081/<request name>
+const adcIp = require('./ipConst')
+var server = app.listen(8081, adcIp, () => {
+    console.log("Server is running.")
+})
